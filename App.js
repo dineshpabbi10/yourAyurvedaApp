dfd10523404f492c47b0910f4a3f45123769af47
Dinesh Pabbi <dineshpabbi10@gmail.com>
2020-12-30 18:19:48 -0800

    add searchbar using nativebase

--- App.js
+++ App.js
@@ -1,17 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Treatments from './Components/Treatments';
 import About from './Components/About';
 import { withSafeAreaInsets } from 'react-native-safe-area-context';
-
+import { Container, Header, Item, Input, Icon, Button, Text } from 'native-base';
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
+      <Container style={{
+        marginTop:23
+      }}>
+        <Header style={{
+            backgroundColor:'green'
+        }} searchBar rounded>
+          <Item>
+            <Icon name="ios-search" />
+            <Input placeholder="Search" />
+          </Item>
+          <Button transparent>
+            <Text>Search</Text>
+          </Button>
+        </Header>
+      </Container>
         <Tab.Navigator 
           tabBarOptions={
             {
--- Components/Treatments.js
+++ Components/Treatments.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+
 
 export default function Treatments() {
   return (
     <View style={styles.container}>
+    
       <Text>Treatments</Text>
       <StatusBar style="auto" />
     </View>
