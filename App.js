15d7eef1b0163270acf518da7de2e3b09894f3c8
Dinesh Pabbi <dineshpabbi10@gmail.com>
2020-12-30 17:15:47 -0800

    basic bottom navigation setup

--- App.js
+++ App.js
@@ -5,15 +5,31 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Treatments from './Components/Treatments';
 import About from './Components/About';
+import { withSafeAreaInsets } from 'react-native-safe-area-context';
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-        <Tab.Navigator>
-            <Tab.Screen name="Home" component={Treatments} />
-            <Tab.Screen name="Settings" component={About} />
+        <Tab.Navigator 
+          tabBarOptions={
+            {
+              style:styles.bottomTab,
+              activeBackgroundColor:'white',
+              activeTintColor:'green',
+              inactiveTintColor:'white',
+              tabStyle:{
+                alignItems:'center',
+                justifyContent:'center',
+                width:100
+              }
+            }
+          }
+        >
+            <Tab.Screen name="Treatments" component={Treatments} />
+            <Tab.Screen name="Treat" component={Treatments} />
+            <Tab.Screen name="About" component={About} />
         </Tab.Navigator>
     </NavigationContainer>
    
@@ -27,4 +43,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  bottomTab:{
+    backgroundColor:'green',
+    display:'flex',
+    alignItems:'center',
+    justifyContent:'center'
+  },
+  labelStyle:{
+    color:'white'
+  },
+  activeBackgroundColor:{
+    backgroundColor:'white'
+  },
+  activeLabelStyle:{
+    color:'white'
+  }
 });
